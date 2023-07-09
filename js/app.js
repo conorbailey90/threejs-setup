7cfcc84c66a68a46345b7d969b2897292c566ed0
conorbailey90 <conbailey90@gmail.com>
2023-07-09 12:29:07 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,86 @@
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+// Shaders
+import fragment from './shaders/fragment.glsl'
+import vertex from './shaders/vertex.glsl'
+
+export default class Sketch{
+    constructor(options){
+        this.time = 0;
+
+        //Scene
+        this.scene = new THREE.Scene();
+
+        // Camera
+        this.camera = new THREE.PerspectiveCamera( 75, this.viewPort.aspectRatio, 0.1, 1000 );
+        this.camera.position.z = 2;
+
+        // Renderer
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( this.viewPort.width, this.viewPort.height);
+        document.body.appendChild( this.renderer.domElement );
+
+        // Controls
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+        this.resize()
+        this.setupResize()
+        this.addObjects()
+        this.render()
+    }
+
+    get viewPort(){
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let aspectRatio = width / height;
+
+        return {
+            width,
+            height, 
+            aspectRatio
+        }
+    }
+
+    setupResize(){
+        window.addEventListener('resize', this.resize.bind(this))
+    }
+
+    resize(){
+        this.width = this.viewPort.width;
+        this.height = this.viewPort.height;
+        this.renderer.setSize(this.width, this.height)
+        this.camera.aspect = this.viewPort.aspectRatio;
+        this.camera.updateProjectionMatrix();
+    }
+
+    addObjects(){
+        this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // this.material = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
+        this.material = new THREE.ShaderMaterial({
+            fragmentShader: fragment,
+            vertexShader: vertex,
+
+        })
+        this.cube = new THREE.Mesh( this.geometry, this.material );
+        this.scene.add( this.cube );
+    }
+
+    render(){
+        this.time += 0.5;
+        this.cube.rotation.x += 0.001;
+	    this.cube.rotation.y += 0.001;
+        this.renderer.render( this.scene, this.camera );
+        window.requestAnimationFrame(this.render.bind(this));
+    }
+}
+
+new Sketch()
+
+
+
+
+
+
+
+
